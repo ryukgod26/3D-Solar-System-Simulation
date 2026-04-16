d43d8e373bb5c78c1be76694e5ded95fee7e720b
raviwhitehat <ekravi26@gmail.com>
2026-04-17 00:36:03 +0530

    Refactor settings namespace to use constexpr for Windows platform

--- Settings.h
+++ Settings.h
@@ -1,5 +1,18 @@
 #pragma once
 
+# ifdef __WIN32__
+
+namespace settings{
+	constexpr int windowWidth = 800;
+	constexpr int windowHeight = 600;
+	constexpr int viewportX = 0;
+	constexpr int viewportY = 0;
+	constexpr int viewportWidth = 800;
+	constexpr int viewportHeight = 600;
+	constexpr char windowTitle[] = "3D Solar System";
+}
+
+#else
 namespace settings{
 	inline constexpr int windowWidth = 800;
 	inline constexpr int windowHeight = 600;
@@ -9,3 +22,4 @@ namespace settings{
 	inline constexpr int viewportHeight = 600;
 	inline constexpr char windowTitle[] = "3D Solar System";
 }
+#endif
