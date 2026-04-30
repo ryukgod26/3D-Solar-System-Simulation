c7faebd2caa50c6f15659233796c781118f2e4a6
root <root@localhost.localdomain>
2026-04-30 19:14:49 +0000

    Maded a small change

--- main.cpp
+++ main.cpp
@@ -9,7 +9,6 @@ int main(){
 		using namespace settings;
 		Game game(windowWidth,windowHeight,viewportX,viewportY,viewportWidth,viewportHeight,windowTitle);
 		while(!game.ShouldClose()){
-
 			game.Tick();
 		}
 		return 0;
