6d57a5b52802c4d082927012deb8c1e8ea0463e3
root <root@localhost.localdomain>
2026-06-11 17:14:41 +0000

    Added header file for Texture Class

---
+++ Texture.h
@@ -0,0 +1,15 @@
+#pragma once
+
+class Texture{
+	public:
+		Texture(const char* texturePath);
+		unsigned int GetID() const;
+		int GetWidth() const;
+		int GetHeight() const;
+		int GetChannelsCount() const;
+	private:
+		unsigned int textureID = -1;
+		int width;
+		int height;
+		int channelsCount;
+};
