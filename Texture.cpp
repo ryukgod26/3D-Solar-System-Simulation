1f7217400c4ea4265ec80defb681411010070fe0
root <root@localhost.localdomain>
2026-06-11 18:58:21 +0000

    Added Texture.cpp which contains the constructors and all the methods of Texture Class

---
+++ Texture.cpp
@@ -0,0 +1,41 @@
+#include "Texture.h"
+#include "glad/glad.h"
+#include "stb_image.h"
+#include <iostream>
+
+Texture::Texture(const char* texturePath){
+	glGenTextures(1, &textureID);
+	glBindTexture(GL_TEXTURE_ID, &textureID);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_FILTER, GL_LINEAR);
+
+	unsigned char* data = stbi_load(texturePath, &width, &height, &channelsCount, 0);
+	if (data){
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	} else{
+		std::cout<< "Failed to load Texture"<< texturePath << std::endl;
+		exit(EXIT_FAILURE);
+	}
+
+	stbi_image_free(data);
+}
+
+unsigned int Texture::GetID() const{
+	return textureID;
+}
+
+int Texture::GetWidth() const{
+	return width;
+}
+
+int Texture::GetHeight() const{
+	return height;
+}
+
+int Texture::GetChannelsCount() const{
+	return channelsCount;
+}
