51afaf4b49e06ca04acd559aa5f60d0eb3695486
root <root@localhost.localdomain>
2026-06-14 07:38:26 +0000

    Added header file for Planet Class

---
+++ Planet.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "Actor.h"
+
+class Planet : public Actor{
+	public:
+		Planet(float orbitRadius, float scale, float orbitSpeedDegrees, float rotationSpeedDegrees);
+		void Update(float deltaTime);
+	private:
+		const float orbitRadius;
+		const float scale;
+		const float orbitSpeedDegrees;
+		const float rotationSpeedDegrees;
+		float currentOrbitDegrees = 0.0f;
+		float currentRotationDegrees = 0.0f;
+}
